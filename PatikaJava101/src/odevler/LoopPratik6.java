7e5c677ac61d5353ebdfb0347966c00289387ddc
Aktan Sanhal <aktansanhal@gmail.com>
2022-03-22 19:20:43 +0300

    LoopPratik6 committed

---
+++ PatikaJava101/src/odevler/LoopPratik6.java
@@ -0,0 +1,30 @@
+package odevler;
+
+import java.util.Scanner;
+
+public class LoopPratik6 {
+
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Sayi giriniz: ");
+		int sayi = scan .nextInt();
+		
+		int temp = sayi;
+		int basamak;
+		int sum=0;
+		
+		while(temp!=0) {
+			basamak = temp%10;
+			sum+=basamak;
+			temp/=10;
+		}
+		
+		System.out.println(sayi + " basamak sayýlarý toplamý: " + sum);
+		
+		
+		
+	}
+
+}
