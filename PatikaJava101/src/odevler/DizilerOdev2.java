60e83b44fa1f1db2dc6c31821ed9c50dc7d5b457
Aktan Sanhal <aktansanhal@gmail.com>
2022-03-29 04:48:30 +0300

    dizilerOdev2 done

--- PatikaJava101/src/odevler/DizilerOdev2.java
+++ PatikaJava101/src/odevler/DizilerOdev2.java
@@ -2,9 +2,39 @@ package odevler;
 
 public class DizilerOdev2 {
 
+	static boolean isRepeated(int[] dublicated,int value) {
+		
+		for(int i: dublicated) {
+			if(i == value) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] array = {10, 20, 20, 10, 10, 20, 5, 20};
+		int[] dublicated = new int[array.length];
+		
+		int count= 0;
+		int startIndex=0;
+		
+		for(int i=0;i<array.length;i++) {
+			for(int j=0;j<array.length;j++) {
+				if(array[i] == array[j]) {
+					count++;
+				}
+			}
+			
+			if(!isRepeated(dublicated, array[i])) {
+				dublicated[startIndex++] = array[i];
+				System.out.println(array[i]+ " sayisi "+ count+ " kere tekrar edildi.");
+			}
+			count=0;
+			
+		}
+				
 	}
 
 }
