6f4d882927dc9798f8724d183bf2641768a1421a
Aktan Sanhal <aktansanhal@gmail.com>
2022-03-30 04:52:32 +0300

    diziOdev3 commit

--- PatikaJava101/src/odevler/DizilerOdev3.java
+++ PatikaJava101/src/odevler/DizilerOdev3.java
@@ -3,8 +3,30 @@ package odevler;
 public class DizilerOdev3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] dizi = {{2,3,4},{5,6,4}};
 
+		int[][] transpoz = new int[dizi[0].length][dizi.length];
+		
+		for(int i=0;i<dizi.length;i++) {
+			for(int j=0;j<dizi[i].length;j++) {
+				transpoz[j][i] = dizi[i][j];
+			}
+		}
+		System.out.println("Matris :");
+		for(int[] row:dizi) {
+			for(int col: row) {
+				System.out.print(col+" ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Transpoze :");
+		for(int[] row:transpoz) {
+			for(int col: row) {
+				System.out.print(col+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
